1f536a374f51763c294da26fa18c2bf3e17796bb
trevorbonsell <trevor.bonsell@sapientrazorfish.com>
2019-05-03 14:41:24 -0400

    Added simple unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import {Home, Error} from './components';
+import { getEndpoint } from './api';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const mockData = {
+    "count": 964,
+    "next": "https://pokeapi.co/api/v2/pokemon/?offset=1&limit=1",
+    "previous": null,
+    "results": [
+        {
+            "name": "bulbasaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/1/"
+        }
+    ]
+}
+
+it('App renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Home renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Home />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Error renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Error />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('api successfully pulls single data item', () => {
+    getEndpoint('https://pokeapi.co/api/v2/pokemon/?limit=1')
+        .then(res => {
+            expect(res).toEqual(mockData);
+        });
 });
--- src/api/index.js
+++ src/api/index.js
@@ -18,6 +18,20 @@ function get(url) {
     return getApi().get(url);
 }
 
+/**
+ * pulls a set number of items from a fixed enpoint
+ * @param  {[Number]} page : page number to request
+ * @return promise
+ */
 export function getPokemon(page) {
     return get(`${API_POKEMON}/?offset=${20 * page - 20}&limit20`);
+}
+
+/**
+ * generic method to retrieve data from any enpoint
+ * @param  {[String]} endpoint : (ie: pokemon/1/, type/3/, ability/4/ etc)
+ * @return promise
+ */
+export function getEndpoint(endpoint) {
+    return get(endpoint);
 }
